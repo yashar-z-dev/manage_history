098508128ee9b50dc5ee2dccfa7286fc7a7531dc
yashar-z-dev <yashar.z.dev@gmail.com>
2025-08-23 17:17:57 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,27 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='manage-history-cli',
+    name='manage-history',
     version='0.1.0',
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     entry_points={
         'console_scripts': [
-            'manage-history = script:main',
+            'clean-history = script:main',
         ],
     },
-    author='your-username',
-    description='A minimal CLI tool to manage shell history',
-    long_description=open('README.md').read(),
+    author='yashar.z.dev',
+    author_email='your.email@example.com',
+    description='A CLI tool to clean bash/zsh history with filters and backup',
+    long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
+    url='https://github.com/yashar-z-dev/manage_history',
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
     ],
     python_requires='>=3.7',
     install_requires=[],
+    include_package_data=True,
 )
